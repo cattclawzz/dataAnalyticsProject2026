433dc63aeb8afca02598b51c074a794e565c579d
cattclawzz <anakinedwards9@gmail.com>
2026-04-04 01:35:35 +0100

    Animation pretty much working just needs some fixes

--- tests/5.2 Line Graph 2 {Single}/animationEg.py
+++ tests/5.2 Line Graph 2 {Single}/animationEg.py
@@ -3,51 +3,59 @@ import matplotlib.pyplot as plt
 from celluloid import Camera
 import pandas as pd
 
+def animatedGraph(title, xLabel, yLabel, xAxis, yAxis, fps):
+
+    print("Please wait...")
+
+    fig = plt.figure(figsize=(10, 6)) 
+    axis = plt.axes(xlim =(0, 6.5), ylim =(0, 8.5)) 
+
+    plt.plot(
+        xAxis, yAxis,
+        marker = 'o',
+        linestyle = '-',
+        color = "white",
+    )
+
+    line, = axis.plot([], [], lw = 5) 
+
+    plt.title(title)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+
+    plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+    plt.tight_layout()       # Prevents label cutoff
+    
+    def init(): 
+        line.set_data([], []) 
+        return line, 
+    
+    xdata, ydata = [], [] 
+    
+    def animate(i): 
+        a = i // fps
+        t = (i % fps) / fps
+
+        if a < len(yAxis):
+            xdata.append(a + t)
+            ydata.append(yAxis[a] + (yAxis[min(a + 1, len(yAxis) - 1)] - yAxis[a]) * t)
+
+        line.set_data(xdata, ydata)
+        
+        return line,
+    
+    anim = animation.FuncAnimation(fig, animate, init_func = init, frames = len(yAxis)*fps, interval = 2, blit = True) 
+    anim.save('animation.gif', writer='pillow', fps=fps)
+
+    print("Program completed")
+
 df = pd.read_csv(r"C:\Users\Anakin\Documents\dataAnalyticsProject2026\tests\5.2 Line Graph 2 {Single}\names.csv")
 
-fig = plt.figure(figsize=(10, 6)) 
-axis = plt.axes(xlim =(0, 6.5), ylim =(0, 8.5)) 
-
-plt.plot(
-    df['name'],
-    df['activityLevel'],
-    marker = 'o',
-    linestyle = '-',
-    label = 'Activity Level'
+animatedGraph(
+    title = "Activity Level by Name",
+    xLabel = "Name",
+    yLabel = "Activity Level",
+    xAxis = df["name"],
+    yAxis = df["activityLevel"],
+    fps = 30,
 )
-
-line, = axis.plot([], [], lw = 2) 
-
-# Labels and Theme
-plt.title("Activity Level by Name")
-plt.xlabel("Name")
-plt.ylabel("Activity Level")
-plt.legend()
-plt.xticks(rotation=45)  # Rotate x-axis labels for readability
-plt.tight_layout()       # Prevents label cutoff
- 
-def init(): 
-    line.set_data([], []) 
-    return line, 
- 
-xdata, ydata = [], [] 
- 
-def animate(i): 
-
-    lst = [1,1,7,5,8,7,3]
-
-    t = i // 30
-
-    x = t
-    y = lst[t]
-     
-    xdata.append(x) 
-    ydata.append(y) 
-    line.set_data(xdata, ydata) 
-    
-    return line,
-   
-anim = animation.FuncAnimation(fig, animate, init_func = init, frames = 7*30, interval = 20, blit = True) 
-anim.save('animation.gif', writer='Pillow', fps=30)
-
-print("Program completed")
