2acff8b496b1c1e9b148db87cf2f5ff7ed177714
cattclawzz <anakinedwards9@gmail.com>
2026-04-06 18:02:00 +0100

    Got text file reading working

---
+++ static/readFIle.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const demo = document.getElementById("demo");
+    const file = demo.getAttribute("data-file");
+
+    fetch(file)
+        .then(x => x.text())
+        .then(y => demo.textContent = y);
+}
