83417b4993a4cbf5f5374e50d185c9ea5f9f87ea
cattclawzz <anakinedwards9@gmail.com>
2026-04-07 04:10:11 +0100

    issue with pillow import

---
+++ tests/PILtest.py
@@ -0,0 +1,17 @@
+import os
+from pillow import Image
+
+def extractFrames(inGif, outFolder):
+    frame = Image.open(inGif)
+    nframes = 0
+    while frame:
+        frame.save( '%s/%s-%s.gif' % (outFolder, os.path.basename(inGif), nframes ) , 'GIF')
+        nframes += 1
+        try:
+            frame.seek( nframes )
+        except EOFError:
+            break;
+    return True
+    
+
+extractFrames('animation.gif', 'output')
